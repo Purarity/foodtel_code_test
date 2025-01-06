c4c18f07648781fcb2813a6e4ff937299d3af59f
Pura <dannyltb55@gmail.com>
2025-01-06 11:58:02 +0100

    added picture to booking page, map to succeeded booking page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 
+import PrismaBuilding from "@/assets/prisma.webp";
 import BookingForm from "@/components/BookingForm";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen text-center flex flex-col">
-      <h1 className="text-5xl font-bold text-center mt-24 mb-16">
-        Foodtel Booking
-      </h1>
-      <BookingForm />
+    <div className="md:flex h-screen">
+      <div className="h-full text-center flex flex-col md:w-1/2 z-10">
+        <h1 className="text-5xl font-bold text-center mt-24 mb-16">
+          Foodtel Booking
+        </h1>
+        <BookingForm />
+      </div>
+      <div className="hidden md:flex w-1/2 overflow-hidden">
+        <Image
+          src={PrismaBuilding}
+          alt="building"
+          className="object-none md:scale-[1.5]"
+        />
+      </div>
     </div>
   );
 }
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,6 +1,6 @@
 export default async function Success() {
   return (
-    <div className="h-screen text-center flex flex-col justify-center">
+    <div className="h-screen text-center flex flex-col justify-center gap-4 px-6">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -15,14 +15,15 @@ export default async function Success() {
           d="m4.5 12.75 6 6 9-13.5"
         />
       </svg>
-
-      <h1 className="">
+      <h1>
         Thank you for booking at Foodtel! We're looking forward to meeting you.
       </h1>
-
-      <a className="" href="/">
-        Back
-      </a>
+      <iframe
+        className="w-full h-[20rem]"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2228.6289267040793!2d12.688317176736337!3d56.042413569647366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465233d3ba914591%3A0x638f0780eecc5b1d!2sHetch!5e0!3m2!1sen!2sse!4v1736156513804!5m2!1sen!2sse"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      />
     </div>
   );
 }
