32e40ac2c66401f0baa4f4be50c2a389e3b97b25
Pura <dannyltb55@gmail.com>
2025-01-07 14:58:01 +0100

    using getMinutes

--- components/BookingList.tsx
+++ components/BookingList.tsx
@@ -227,7 +227,7 @@ export default function BookingList({
                   <td className="break-words">{booking.email}</td>
                   <td>{booking.totalGuests}</td>
                   <td>
-                    {new Date(booking.time).getHours()}{" "}
+                    {new Date(booking.time).getMinutes()}{" "}
                     {new Intl.DateTimeFormat("en-GB", {
                       dateStyle: "short",
                     }).format(booking.time)}
